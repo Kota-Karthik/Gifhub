4c0d0dff88ba096482751ca42bed4bde6596ff57
kota-karthik <kotakarthik2307@gmail.com>
2024-07-06 15:13:20 +0530

    routed all pages

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+
+import './App.css'
+import { createBrowserRouter } from 'react-router-dom'
+import AppLayout from './layouts/app-layout'
+import Home from "./pages/home"
+import Category from './pages/category'
+import Favorites from './pages/favourites'
+import Search from './pages/search'
+import GifPage from './pages/single-gif'
+
+const router=createBrowserRouter([
+  {
+    element:<AppLayout/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>,
+      },
+      {
+        path:'/:category',
+        element:<Category/>,
+      },
+      {
+        path:'/search/:query',
+        element:<Search/>,
+      },
+      {
+        path:'/:type/:slug',
+        element:<GifPage/>,
+      },
+      {
+        path:'/favourites',
+        element:<Favorites/>,
+      },
+    ]
+  }
+])
+
+function App() {
+
+  return (
+    <>
+      <div>hello world!</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/single-gif.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const GifPage = () => {
+  return (
+    <div>GifPage</div>
+  )
+}
+
+export default GifPage;
