583f9511b6c6b1256dee13edaaad5b8a72d5c1f5
kota-karthik <kotakarthik2307@gmail.com>
2024-07-07 10:16:06 +0530

    completely styled the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { HiEllipsisVertical, HiMiniBars3BottomRight } from 'react-icons/hi2'
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+  const [showCategories, setShowCategories] = useState(false);
+
+
   return (
-    <div>Header</div>
+    <nav>
+      <div className='relative flex gap-4 justify-between items-center mb-2'>
+        <Link to='/' className='flex gap-2'>
+          <img src="/logo.svg" className='w-8' alt="GifHub logo" />
+          <h1 className='text-5xl font-bold tracking-tight cursor-pointer'>
+            GIFHUB
+          </h1>
+        </Link>
+        {/*render categories */}
+        <div className='font-bold text-md flex gap-2 items-center'>
+
+
+          <Link className='px-4 py-1 hover:gradient border-b-4 hidden lg:block '>Reactions</Link>
+          <button onClick={() => setShowCategories(!showCategories)}>
+            <HiEllipsisVertical
+              size={35}
+              className={`py-0.5 hover:gradient ${showCategories ? "gradient" : ""
+                }
+          border-b-4 hidden lg:block`}
+            />
+          </button>
+          <div className='h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded'>
+            <Link to='/favourites'>
+              Favourite GIFs
+            </Link>
+          </div>
+          <button>
+            <HiMiniBars3BottomRight
+              className='text-sky-400 block lg-hidden xl:hidden '
+              size={30} />
+          </button>
+        </div>
+        {showCategories &&
+          (
+            <div className='absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient z-20'>
+              <span>Categories</span>
+              <hr />
+              <div>
+                <Link className='font-bold'>Reactions</Link>
+              </div>
+            </div>
+
+          )
+        }
+      </div>
+        {/* search */}
+    </nav>
   )
 }
 
