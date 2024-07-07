a1db98ee741ca95fe389fa8e60608f6824df23d1
kota-karthik <kotakarthik2307@gmail.com>
2024-07-07 13:16:49 +0530

    added Gif component and function to render trending gifs

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { GifState } from '../context/git-context';
+import Gif from '../components/gif';
 
 export const Home = () => {
+  const { gf, filter, setFilter, setGifs,gifs } = GifState();
+  const fetchTrendingGifs=async()=>{
+    const {data}=await gf.trending({
+      limit:20,
+      type:filter,
+      rating:"r",
+    });
+
+    setGifs(data);
+  }
+
+  useEffect(()=>{
+    fetchTrendingGifs();
+  },[filter])
+
   return (
-    <div>Home</div>
+    <div>
+      <img src="/banner.gif" alt="earth banner" className='mt-2 rounded w-full' />
+      {/* <FilterGif/> */}
+      <div className='columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2'>
+        {gifs.map((gif)=>{
+          return <Gif gif={gif} key={gif.title}/>
+        })}
+      </div>
+    </div>
   )
 }
 
